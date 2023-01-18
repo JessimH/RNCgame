0aef552ae2b11ad05912ebe017b19d9aeb997845
Jessim <jessim.heddadi@influence4you.com>
2023-01-18 16:05:13 +0100

    Project structure

---
+++ screens/StartGameScreen.js
@@ -0,0 +1,11 @@
+import { View, TextInput, Button } from 'react-native';
+
+function StartGameScreen() {
+    return (
+        <View>
+            <TextInput placeholder="Enter a Number" />
+        </View>
+    )
+}
+
+export default StartGameScreen; // Export the component so it can be used in other files
