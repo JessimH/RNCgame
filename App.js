16b8ba1ff6b9832fbdb717b263977097c2d5f958
Jessim <jessim.heddadi@influence4you.com>
2023-01-20 12:25:00 +0100

    linear gradient

--- App.js
+++ App.js
@@ -1,13 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import StartGameScreen from './screens/StartGameScreen';
+import { LinearGradient } from 'expo-linear-gradient';
 
 export default function App() {
   return (
-    <StartGameScreen />
+    <LinearGradient
+      style={styles.rootScreen}
+      colors={['#ffedff', '#ffe3ff']}
+    >
+      <StartGameScreen />
+    </LinearGradient >
   );
 }
 
 const styles = StyleSheet.create({
-
+  rootScreen: {
+    flex: 1,
+  }
 });
