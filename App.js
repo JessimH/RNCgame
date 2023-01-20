969c1f14e2a3641a03cc2dddcf9f5f94e40462b7
Jessim <jessim.heddadi@influence4you.com>
2023-01-20 12:35:13 +0100

    BackgroundImage RN

--- App.js
+++ App.js
@@ -1,15 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, ImageBackground } from 'react-native';
 import StartGameScreen from './screens/StartGameScreen';
 import { LinearGradient } from 'expo-linear-gradient';
+
+const backgroundImage = require('./assets/images/background.png');
 
 export default function App() {
   return (
     <LinearGradient
       style={styles.rootScreen}
-      colors={['#ffedff', '#ffe3ff']}
+      colors={['#4e0329', '#ddb52f']}
     >
-      <StartGameScreen />
+      <ImageBackground
+        source={backgroundImage}
+        style={styles.rootScreen}
+        resizeMode='cover'
+        imageStyle={styles.backgroundImage}
+      >
+        <StartGameScreen />
+      </ImageBackground>
     </LinearGradient >
   );
 }
@@ -17,5 +26,8 @@ export default function App() {
 const styles = StyleSheet.create({
   rootScreen: {
     flex: 1,
+  },
+  backgroundImage: {
+    opacity: 0.2
   }
 });
